b7b931746044316c737a5cdb8d9c5601698adc73
BenBurchfield <BenBurchfield@gmail.com>
2023-11-15 23:08:17 -0600

    Init Commit

---
+++ Logging.sh
@@ -0,0 +1,23 @@
+!# /etc/usr/bash
+
+# INSTRUCTIONS - YOU MUST READ #
+# GenerativeAI, ChatGPT specifically CAN and should be used on this section of the Exam #
+# You will not be able to complete this in a timely manner unless you use GenerativeAI
+
+# There is no limitation on prompts that can be used here
+
+# The bash script must perform the following:
+# Interface with the URL below.
+# Iterate across the returned results and 
+# append a file in your home directory when ran from anywhere 
+# inside the file you should create a list of PunchIDs, their Type, and the PunchDateTime on a single line
+# formated similar to this:
+# PunchID: 260f362c6a4d | PunchType: Clock In | DateTimeOccured: 2023-10-24 17:07:02 | RecordedToLog: 16Nov2023
+# The final section on the line above shoudl be the current date in that format
+
+strURL = https://www.thesimplehomestead.com/simpletime/time.php?SessionID=eb5a01f8-7959-47b5-ba26-fbb0884ee82b
+
+# Correct any issues in this file and append your code here.
+
+
+# This section of the exam is worth 10 pts
